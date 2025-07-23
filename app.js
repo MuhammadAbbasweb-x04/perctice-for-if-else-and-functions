078cd184498875bcb7774a714cfa7de9959a91c6
MuhammadAbbas-web04 <mabbasmshahi2003@gmail.com>
2025-07-23 16:02:37 +0500

    added

---
+++ app.js
@@ -0,0 +1,13 @@
+// var marks= parseInt(prompt("enter your marks"));
+// if( marks >=90 && marks <=100 ){
+// console.log("Distinction");
+// } else if ( marks >=60 && marks <=80 ){
+//     console.log("Pass");
+// } else if( marks >= 30 && marks <= 40 ){
+// console.log("Fail");
+// } else {
+//     console.log("Invalid Marks");
+// }
+
+
+
