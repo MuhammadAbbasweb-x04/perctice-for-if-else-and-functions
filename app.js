4d2ed650009190a6dd326f210c8bc10547d442bb
MuhammadAbbas-web04 <mabbasmshahi2003@gmail.com>
2025-07-23 16:44:07 +0500

    added

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
+
+// ✅ Question 4: Number Sign Checker
+// Ask the user to input a number and display:
+
+// "Positive" if number > 0
+
+// "Negative" if number < 0
+
+// "Zero" if number == 0
+
+
+
+
+
+
+// ✅ Question 5: Largest of Three Numbers
+// Ask the user to input three numbers and determine which one is the largest.
+
+
+
+
+
+
+
+
+
+
+
+
+
 // var marks= parseInt(prompt("enter your marks"));
 // if( marks >=90 && marks <=100 ){
 // console.log("Distinction");
@@ -10,4 +40,26 @@
 // }
 
 
+// var number= parseInt(prompt("enter your number "));
+// if ( number >=22 && number <=24  ){
+// console.log("odd");
+// } else{
+// console.log("Even");
+// }
 
+
+
+
+// var age = parseInt(prompt( "enter your age to Eligible to vote" ));
+// if ( age >=18 && age <= 30 ){
+// console.log("Eligible to vote");
+// } else if ( age >= 13 && age <=17  ){
+// console.log("Not eligible to vote");
+// } else{
+//     console.log("Invalid age");
+// }
+
+
+function city (user){
+
+}
