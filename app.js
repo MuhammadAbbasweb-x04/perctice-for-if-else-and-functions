93384c68bde857609561e6f13c96940724681a37
MuhammadAbbas-web04 <mabbasmshahi2003@gmail.com>
2025-07-24 16:15:41 +0500

    added

--- app.js
+++ app.js
@@ -59,7 +59,62 @@
 //     console.log("Invalid age");
 // }
 
+// let number= parseInt(prompt("check your number isa Positive and Negative "));
+// if( number > 0  ){
+// console.log("Positive");
+// } else if ( number < 0 ){
+// console.log("Negative");
+// } else{
+//     console.log("Zero");
+// }
 
-function city (user){
 
+// var number = parseInt(prompt("  enter your number "));
+// if( number >= 10000 && number <= 1000000000000000    ){
+// console.log("Largest number");
+// } else if ( number >= 1 && number <= 1000 ){
+//     console.log("Smallest number");
+// } else{
+//     console.log("invild  number  " );
+// }
+
+
+
+// var marks=parseInt(prompt("enter your marks "));
+// if( marks >= 80 && marks <= 100 ){
+// console.log("Distinction");
+// } else if ( marks >= 50 && marks <= 75 ){
+// console.log("Pass");
+// } else if ( marks >= 30 && marks <= 40  ){
+// console.log("Fail")
+// } else {
+//     console.log("inviled number");
+// }
+
+
+
+
+// var discount= parseInt(prompt( " enter your age and check your Discount Eligibility" ));
+// if( discount >= 70 &&  discount <= 100 ){
+// console.log("Senior Premium Discount");
+// } else if ( discount >= 50 && discount <= 60 ){
+// console.log("Senior Discount");
+// } else if ( discount >=30 && discount <= 40 ){
+//     console.log("Premium Discount");
+// } else{
+//     console.log("No discount");
+// }
+ 
+
+var product = parseInt(prompt("enter your product price "));
+if( product >=900 && product <= 1000 ){
+console.log("Buy now with discount");
+} else if( product >= 500 && product <= 800 ){
+console.log("Buy now");
+} else if (   product === 0){
+console.log("Out of stock");
+} else{
+    console.log("inviled number");
 }
+
+
